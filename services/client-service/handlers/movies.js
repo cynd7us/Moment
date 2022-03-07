d62249f579ad7ad4f81fc723f4a57cd28f973e77
user.name <zohar.yzgaev@healthy.io>
2022-03-07 14:33:07 +0200

    Fix movies handler

--- services/client-service/handlers/movies.js
+++ services/client-service/handlers/movies.js
@@ -1,21 +1,9 @@
 const { TheMovieDbApiClient } = require('../../../infra/api-clients');
-const cache = require('../../../infra/cache');
 
 const getMostRatedMovies = async () => {
-  const myValue = await cache.set({
-    key: 'russia',
-    value: { key: 'this is the value of the key' },
-    ttl_priority: 'low',
-  });
-  console.log(myValue);
-  const myGetValue = await cache.get({ key: 'russia', isJson: true });
-  console.log(myGetValue);
-  /*
-    await cache.set({ key: 'myKey', value: 'this is a test', ttl_priority: 'medium'});
-    const MoviesApiClient = TheMovieDbApiClient.getInstance();
-    const movies = await MoviesApiClient.getMostRated();
-    return movies;
-    */
+  const MoviesApiClient = TheMovieDbApiClient.getInstance();
+  const movies = await MoviesApiClient.getMostRated();
+  return movies;
 };
 
 const getPopularMovies = async () => {
